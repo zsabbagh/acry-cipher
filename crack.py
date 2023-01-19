a59909659f61d6e108ab9ac1a6d9a0d67d9f076f
zsabbagh <zassabbagh@gmail.com>
2023-01-19 16:12:35 +0100

    Add more

--- crack.py
+++ crack.py
@@ -1,1 +1,52 @@
-alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_#'
+"""
+    Cipher
+"""
+import sys
+import os
+import re
+import time # .sleep(), .time(), .time_ns()
+
+"""
+    Ideas:
+    - Statistical analysis
+"""
+
+ALPHA = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_#'
+DEFAULT = '1.data'
+MOST_COMMON = 'ETAOINSHRDLUCWMFYGPBVKXJQZ'
+
+def count(d, k):
+    if k not in d:
+        d[k] = 1
+    else:
+        d[k] += 1
+
+def main():
+    args = sys.argv[1:]
+    path = None
+
+    for i in range(0, len(args)):
+        f = args[i]
+        if f[0] != '-':
+            path = f
+
+    if path is None:
+        path = DEFAULT
+
+    file = open(path, 'r').read().rstrip()
+    file = ''.join(file.split('\n'))
+
+    dct = {}
+    for c in file:
+        count(dct, c)
+
+    ls = []
+    for c in dct:
+        ls.append((c, dct[c]))
+
+    ls = sorted(ls, key=lambda x : x[1])
+    for (c, v) in ls:
+        print(c + ": " + str(v))
+
+if __name__ == "__main__":
+    main()
